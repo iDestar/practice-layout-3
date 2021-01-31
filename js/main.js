5a1cb971e9586bafa90b1e401366c0c6a0c4416e
iDestar <destarpwnz@gmai.com>
2021-01-31 15:14:12 +0300

    sum

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+const burger = document.querySelector('.header__burger');
+const siteNav = document.querySelector('.site__nav');
+const closeNav = document.querySelector('.nav__list-close');
 
+const burgerHandler = function () {
+  siteNav.classList.add('site__nav-acite');
+};
+
+const closeNavHandler = function () {
+  siteNav.classList.remove('site__nav-acite');
+};
+
+burger.addEventListener('click', burgerHandler);
+closeNav.addEventListener('click', closeNavHandler);
